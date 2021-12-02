c6c7ec6a2bf8c21a6c14878c7aa9b70256c0eb05
Marcelo <marsleite@gmail.com>
2021-12-02 09:57:28 -0300

    testando na pratica a linguagem java

---
+++ Modulo_01/dia_01/pacote01/introdução/src/br/taferas/Tarefa.java
@@ -0,0 +1,34 @@
+package br.taferas;
+
+public class Tarefa {
+  String descricao;
+  int horas;
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public void setHorasEstudo(int horas) {
+    this.horas = horas;
+  }
+
+  public void executar() {
+    System.out.println("Executando tarefa: " + descricao);
+  }
+
+  public void finalizar() {
+    System.out.println("Finalizando tarefa: " + descricao);
+  }
+
+  public void cancelar() {
+    System.out.println("Cancelando tarefa: " + descricao);
+  }
+
+  public int tamanhoDescricao(){
+    return descricao.length();
+  }
+}
